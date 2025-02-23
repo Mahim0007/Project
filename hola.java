d33dff46ed25f7dc11837ee18ff1d771cac2d3a1
Mahim0007 <165550668+Mahim0007@users.noreply.github.com>
2025-02-24 00:14:23 +0600

    Add files via upload

---
+++ hola.java
@@ -0,0 +1,13 @@
+public class hola
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Total of Sum = ");
+        int num1,num2,num3,sum;
+        num1=5;
+        num2=6;
+        num3=8;
+        sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
